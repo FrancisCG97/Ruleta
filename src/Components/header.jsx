39b24c1999be25b6e832e29d88e78cdd52aaa5fb
Francis <f.carrasco.g.07@gmail.com>
2023-08-11 09:48:58 -0400

    Home.css, Home.jsx and header.jsx files were added in Components file. Header styles were modified in Home.css.

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -5,7 +5,20 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 const Header = () => {
 
     return (
-        <h1>Ruleta al azarrrr</h1>
+        <>
+            <button className="btn btn-lg" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasTop" aria-controls="offcanvasTop"> ¿Cómo funciona la Ruleta al azar? </button>
+
+            <div className="offcanvas offcanvas-top" tabIndex="-1" id="offcanvasTop" aria-labelledby="offcanvasTopLabel">
+                <div className="offcanvas-header">
+                    <h5 className="offcanvas-title" id="offcanvasTopLabel"> ¿Cómo funciona? </h5>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body">
+                    La Ruleta al azar funciona...
+                </div>
+            </div>
+            <h1>Ruleta al azar</h1>
+        </>
     )
 }
 
