81a2c1eb740c49f1953d0c473778bb745db3c78a
sebastian <sespinozahenriquez@gmail.com>
2023-08-15 11:28:54 -0400

    ajustes back

--- src/Components/roulette.jsx
+++ src/Components/roulette.jsx
@@ -3,31 +3,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import React from 'react';
 import { useState } from "react";
-<<<<<<< HEAD
 import { Wheel } from 'react-custom-roulette'
 import { ToastContainer, toast } from 'react-toastify';
 import XLSX from 'xlsx';
 
 const Roulette = () => {
-=======
-import { Wheel } from 'react-custom-roulette';
-
-export default function Roulette() {
-
-    //FUNCIÓN PARA PASAR LA DATA A LA RULETA
-    const data = [
-        { option: '0' },
-        { option: '1' },
-        { option: '2' },
-        { option: '3' },
-        { option: '4' },
-        { option: '5' },
-    ]
-
->>>>>>> 59d4a5c4d0eabd87c94c574b28a60007edb7089e
     const [mustSpin, setMustSpin] = useState(false);
     const [prizeNumber, setPrizeNumber] = useState(0);
     const [excelData, setExcelData] = useState([]);
+    const [mostrarRuleta, setMostrarRuleta] = useState(false);
 
     const handleFileChange = async (event) => {
         console.log("funcione")
@@ -45,6 +29,7 @@ export default function Roulette() {
           const parsedData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
     
           setExcelData(parsedData);
+          setMostrarRuleta(true)
         };
     
         reader.readAsArrayBuffer(file);
@@ -58,7 +43,6 @@ export default function Roulette() {
         }
     }
 
-<<<<<<< HEAD
     console.log(excelData)
 
     const parsedData = excelData.slice(1).map(row => ({
@@ -77,9 +61,6 @@ export default function Roulette() {
           }));
 
     //TOAST DE PRUEBA
-=======
-    //ALERT DE PRUEBA
->>>>>>> 59d4a5c4d0eabd87c94c574b28a60007edb7089e
     const msje = () => {
         alert('Funciona');
     }
@@ -89,7 +70,7 @@ export default function Roulette() {
             <div className="container" id="wheel">
                 <div className="row" id="wheel-row">
                     <div className="col-10 col-sm-10 col-md-6 col-lg-6 col-xl-6" id="grids">
-                        <Wheel
+                        {mostrarRuleta == true && <Wheel
                             mustStartSpinning={mustSpin}
                             prizeNumber={prizeNumber}
                             data={data}
@@ -97,19 +78,10 @@ export default function Roulette() {
                             onStopSpinning={() => {
                                 setMustSpin(false);
                             }}
-                        />
-<<<<<<< HEAD
-                        <div className="container" id="buttons-container">
-                            <button className="btn" onClick={handleSpinClick} id="spin"> ¡LANZAR LA RULETA! </button>
-                            <div className="card" id="input-file">
-                                <div className="card-body">
-                                    <div className="mb-3">
-                                        <label htmlFor="formFile" className="form-label"> Arrastra aquí un archivo para subir los datos
-                                            <img src="./src/Images/upload.png" className="img-thumbnail" alt="upload image">
-                                            </img>
-                                        </label>
-                                        <input className="form-control" type="file" id="formFile" onChange={handleFileChange}></input>
-=======
+                            />}
+
+
+
                         </div>
                         <div className="col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6" id="button-grids">
                             <div className="container" id="buttons-container">
@@ -121,18 +93,12 @@ export default function Roulette() {
                                                 <img src="./src/Images/upload.png" className="img-thumbnail" alt="upload image">
                                                 </img>
                                             </label>
-                                            <input className="form-control" type="file" id="formFile"></input>
+                                            <input className="form-control" type="file" id="formFile" onChange={handleFileChange}></input>
                                         </div>
->>>>>>> 59d4a5c4d0eabd87c94c574b28a60007edb7089e
                                     </div>
                                 </div>
                                 <button className="btn" onClick={msje} id="return-original"> VOLVER A ORIGINAL </button>
                             </div>
-<<<<<<< HEAD
-                            <button className="btn" onClick={msje} id="return-original"> REINICIAR EL CONTENIDO </button>
-                            <pre>{JSON.stringify(excelData, null, 2)}</pre>
-=======
->>>>>>> 59d4a5c4d0eabd87c94c574b28a60007edb7089e
                         </div>
                     
                 </div>
@@ -140,4 +106,4 @@ export default function Roulette() {
         </>
     )
 }
-
+export default Roulette;
